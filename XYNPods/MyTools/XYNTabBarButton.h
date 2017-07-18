dc2a2c8d8aa4087958afdf38cf0c90819f68cf82
DingJia <dingjia-PengP@AP-2.local>
2017-07-18 17:22:30 +0800

    修改路径

---
+++ XYNPods/MyTools/XYNTabBarButton.h
@@ -0,0 +1,13 @@
+//
+//  XYNTabBarButton.h
+//  XDPodDemo
+//
+//  Created by DingJia on 2017/5/16.
+//  Copyright © 2017年 席亚楠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYNTabBarButton : UIButton
+@property (nonatomic, strong) UITabBarItem *item;
+@end
